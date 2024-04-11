393ac317c7a03184104d560c71b15fa9ad442604
Loi <129076315+JBL-987@users.noreply.github.com>
2024-04-11 13:00:23 +0700

    Update index.js

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -24,80 +24,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:date?_string", function(req, res) {
-  res.json({ unix: "date?_String", utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+app.get("/api", function (req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
 });
 
-// Mengambil parameter
-let date_string = req.params.date_string;
+app.get("/api/:timestamp", function (req, res) {
+  const timestamp = req.params.timestamp;
 
-// Mengambil data tanggal sekarang
-let now = new Date();
+  if (!isNaN(Number(timestamp)) && timestamp.length === 13) {
+    return res.json({
+      unix: timestamp,
+      utc: new Date(Number(timestamp)).toUTCString(),
+    });
+  }
+  
+  if (new Date(timestamp).toUTCString() !== "Invalid Date") {
+    return res.json({
+      unix: new Date(timestamp).getTime(),
+      utc: new Date(timestamp).toUTCString(),
+    });
+  }
 
-// Pengecekan bila parameter tidak dicantumkan
-if (!date_string) {
-  return res.json({
-    unix: now.getTime(),
-    utc: now.toUTCString(),
-  })
-}
-
-// Regex
-let pattern1 = /^\d{4}-\d{2}-\d{2}$/; // 2015-12-25
-let pattern2 = /^\d+$/; // 1451001600000
-let pattern3 = /^\d{2}\s(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{4}?/; // 05 October 2015
-
-// Inisiasi unix dan utc
-let unix;
-let utc;
-
-// Jika cocok dengan regex pertama atau ketiga
-if (date_string.match(pattern1) || date_string.match(pattern3)) {
-  // mendapatkan waktu sesuai dengan input
-  let date = new Date(date_string);
-
-  // Mendapatkan nilai unix (detik sampai saat itu)
-  unix = date.getTime();
-
-  // Mendapatkan nilai string utc
-  utc = date.toUTCString();
-
-  // Mengembalikan json berisi unix dan utc
-  return res.json({
-    unix: Number(unix),
-    utc: utc,
-  })
-}
-
-// Jika cocok dengan regex kedua
-if (date_string.match(pattern2)) {
-  // mendapatkan waktu sesuai dengan input yang di konversi ke number dahulu
-  let dateObj = new Date(Number(date_string));
-
-  // Mendapatkan nilai unix dari input
-  unix = Number(date_string);
-
-  // Mendapatkan nilai utc
-  let utc = dateObj.toUTCString();
-
-  // Mengembalikan json berisi unix dan utc
-  return res.json({
-    unix: unix,
-    utc: utc,
-  })
-}
-
-// Jika parameter tidak sesuai
-return res.json({
-  'error': 'Invalid Date',
-})
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  res.json({ error: "Invalid Date" });
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000);
 });
